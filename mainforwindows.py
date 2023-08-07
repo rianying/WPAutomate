5b83635694342f7bd4afaabc2d5ce6d1ae445212
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2023-08-07 21:03:20 +0700

    overall update
    Updated mainformac.py and mainforwindows.py and added Customer Data to cross reference in the future update

--- mainforwindows.py
+++ mainforwindows.py
@@ -2,50 +2,71 @@ import pandas as pd
 import numpy as np
 import tkinter as tk
 from tkinter import filedialog, messagebox
+import random
 
 # Helper function to calculate business days
 def add_business_days(from_date, add_days):
     return np.busday_offset(from_date, add_days, roll='forward')
 
 def generate_queries(filepath):
-    df = pd.read_excel(filepath)
-    value_clauses = []  # We will store the individual value clauses here
+    df = pd.read_csv(filepath)  # Updated this line to read from CSV
+
+    value_clauses_preorder = []
+    value_clauses_order_checking_start = []
+    value_clauses_order_checking_finish = []
 
     for _, row in df.iterrows():
-        no_PO = row['No PO'] if not pd.isna(row['No PO']) else ""
-        no_SO = row['No SO'] if not pd.isna(row['No SO']) else ""
-        customer_name = row['Customer Name'] if not pd.isna(row['Customer Name']) else ""
-        order_time = row['Order Time'] if not pd.isna(row['Order Time']) else ""
+        no_PO = row['no_PO'] if not pd.isna(row['no_PO']) else ""
+        no_SO = row['no_SO'] if not pd.isna(row['no_SO']) else ""
+        customer_name = row['customer_name'] if not pd.isna(row['customer_name']) else ""
+        order_time = row['order_time'] if not pd.isna(row['order_time']) else ""
 
         if "CRB" in no_SO or "BDG" in no_SO:
             po_expired = add_business_days(pd.to_datetime(order_time).date(), 7)
         else:
             po_expired = add_business_days(pd.to_datetime(order_time).date(), 4)
 
-        value_clause = f'("{no_PO}", "{no_SO}", "{customer_name}", \'{order_time}\', \'{po_expired}\')'
-        value_clauses.append(value_clause)
+        value_clause_preorder = f'("{no_PO}", "{no_SO}", "{customer_name}", \'{order_time}\', \'{po_expired}\')'
+        value_clauses_preorder.append(value_clause_preorder)
 
-    values_str = ',\n'.join(value_clauses)  # Join all value clauses together, separated by commas
-    query = f'INSERT INTO preorder(no_PO, no_SO, customer_name, order_time, po_expired) VALUES {values_str};'
+        fat_random_minutes = random.randint(1, 10)
+        fat_start_time = (pd.to_datetime(order_time) + pd.Timedelta(minutes=fat_random_minutes)).strftime('%Y-%m-%d %H:%M:%S')
+        value_clause_order_checking_start = f'("{no_SO}", \'{fat_start_time}\', "{customer_name}")'
+        value_clauses_order_checking_start.append(value_clause_order_checking_start)
 
-    return query
+        no_SJ = no_SO.replace("SO", "SJ")
+        fat_random_minutes = random.randint(1, 10)
+        fat_checking_finish = (pd.to_datetime(fat_start_time) + pd.Timedelta(minutes=fat_random_minutes)).strftime('%Y-%m-%d %H:%M:%S')
+        value_clause_order_checking_finish = f'("{no_SO}", "{no_SJ}", \'{fat_checking_finish}\', "{customer_name}", "Process")'
+        value_clauses_order_checking_finish.append(value_clause_order_checking_finish)
 
+    values_str_preorder = ',\n'.join(value_clauses_preorder)
+    values_str_order_checking_start = ',\n'.join(value_clauses_order_checking_start)
+    values_str_order_checking_finish = ',\n'.join(value_clauses_order_checking_finish)
+
+    query_preorder = f'INSERT INTO preorder(no_PO, no_SO, customer_name, order_time, po_expired) VALUES {values_str_preorder};'
+    query_order_checking_start = f'INSERT INTO order_checking_start(no_SO, start_check, customer_name) VALUES {values_str_order_checking_start};'
+    query_order_checking_finish = f'INSERT INTO order_checking_finish(no_SO, no_SJ, FAT_checking_finish, customer, status_FAT) VALUES {values_str_order_checking_finish};'
+
+    return query_preorder, query_order_checking_start, query_order_checking_finish
 
 def pick_file():
-    filepath = filedialog.askopenfilename()
+    # Updated the file type to CSV
+    filepath = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
     file_path.set(filepath)
     lbl_filepath.config(text=filepath)
 
 def execute():
     filepath = file_path.get()
     if not filepath:
-        messagebox.showerror("Error", "Please select an Excel file first!")
+        messagebox.showerror("Error", "Please select a CSV file first!")
         return
 
     try:
-        queries = generate_queries(filepath)
+        queries_preorder, queries_order_checking_start, queries_order_checking_finish = generate_queries(filepath)
+        queries_combined = f"{queries_preorder}\n\n{queries_order_checking_start}\n\n{queries_order_checking_finish}"
         app.clipboard_clear()
-        app.clipboard_append(queries)
+        app.clipboard_append(queries_combined)
         app.update()  # To ensure the clipboard content is updated
         messagebox.showinfo("Notification", "Generated SQL Queries have been copied to the clipboard!")
     except Exception as e:
@@ -59,7 +80,7 @@ file_path = tk.StringVar()
 frame = tk.Frame(app)
 frame.pack(pady=20)
 
-btn_pick_file = tk.Button(frame, text="Pick an excel file", command=pick_file)
+btn_pick_file = tk.Button(frame, text="Pick a CSV file", command=pick_file)  # Updated the button text
 btn_pick_file.grid(row=0, column=0, padx=10)
 
 btn_execute = tk.Button(frame, text="Generate PO Query", command=execute)
@@ -69,4 +90,3 @@ lbl_filepath = tk.Label(frame, text="")
 lbl_filepath.grid(row=1, column=0, columnspan=2, pady=10)
 
 app.mainloop()
-
