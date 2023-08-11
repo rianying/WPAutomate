7e91153c8081c4ac60ab73982bf0ec094dd54ad6
rianying <riansshole124@gmail.com>
2023-08-11 15:58:12 +0700

    update

--- fetch_PO.py
+++ fetch_PO.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 def process_csv(file_path):
     # 1. Read the csv file
-    df = pd.read_csv(file_path, sep=';')
+    df = pd.read_csv(file_path, sep=',')
     
     # 3. Take only TGL SO, NO. SO, and NAMA PELANGGAN
-    df = df[["TGL SO", "NO. SO", "NAMA PELANGGAN"]]
+    df = df[["order_date", "no_PO", "no_SO", "customer_name"]]
     
     # 2. Remove duplicates based on the specific columns
     df = df.drop_duplicates()
@@ -17,12 +17,12 @@ def process_csv(file_path):
         "Sep": "Sep", "Okt": "Oct", "Nov": "Nov", "Des": "Dec"
     }
     for non_eng, eng in month_translations.items():
-        df["TGL SO"] = df["TGL SO"].str.replace(non_eng, eng)
+        df["order_date"] = df["order_date"].str.replace(non_eng, eng)
     
     # 4. Change TGL SO to date time and asks user input for the time.
     time_input = input("Please enter the time (format HH:MM:SS): ")
-    df["TGL SO"] = pd.to_datetime(df["TGL SO"], format='%d %b %Y') 
-    df["TGL SO"] = df["TGL SO"].dt.strftime('%Y-%m-%d') + ' ' + time_input
+    df["order_date"] = pd.to_datetime(df["order_date"], format='%d %b %Y') 
+    df["order_date"] = df["order_date"].dt.strftime('%Y-%m-%d') + ' ' + time_input
     
     # 5. Asks user input for NO. SO range start
     start_range = input("Please enter the start of the NO. SO range: ")
@@ -31,10 +31,10 @@ def process_csv(file_path):
     finish_range = input("Please enter the finish of the NO. SO range: ")
     
     # Filter the dataframe based on the NO. SO range provided by the user
-    result_df = df[df['NO. SO'].between(start_range, finish_range)]
+    result_df = df[df['no_SO'].between(start_range, finish_range)]
     
     # 7. Print the result
-    print(result_df)
+    result_df.to_csv('PO_fetched.csv', index=False)
 
 # Call the function with the path to your CSV file
-process_csv("so.csv")
+process_csv("SO.csv")
