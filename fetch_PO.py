2c89026e2580e4a4121228946ccee7bb16f2972a
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2023-08-14 20:42:47 +0700

    customerNameRenameon-progress

--- fetch_PO.py
+++ fetch_PO.py
@@ -4,8 +4,8 @@ import os
 def clean():
     so = r'/Volumes/PUBLIC/SC - Samuel (Intern)/SO.csv'
     data = pd.read_csv(so, sep=';', skiprows=4)
-    data.rename(columns={'Tgl Pesan': 'order_date','Unnamed: 2': 'no_SO','Unnamed: 4': 'customer_name', 'Unnamed: 6': 'no_PO'}, inplace=True)
-    selected = data[['order_date', 'no_PO', 'no_SO', 'customer_name']]
+    data.rename(columns={'Tgl Pesan': 'order_date','Unnamed: 2': 'no_SO','No. Pesanan': 'customer_number', 'Unnamed: 4': 'customer_name', 'Unnamed: 6': 'no_PO'}, inplace=True)
+    selected = data[['order_date', 'no_PO', 'no_SO', 'customer_name', 'customer_number']]
     cleaned = selected[selected['no_SO'].notna()]
     cleaned.to_csv('SO_cleaned.csv', index=False)
     print('SO.csv has been cleaned!')
