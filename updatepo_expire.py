9cb95297db9f426736555b565c7deb3e95132465
rianying <riansshole124@gmail.com>
2023-08-23 09:27:12 +0700

    update

--- updatepo_expire.py
+++ updatepo_expire.py
@@ -2,6 +2,7 @@ import csv
 import datetime
 import subprocess
 import pyperclip
+import os
 
 # Read po_expire.csv and create a dictionary of customer names and corresponding po_expire values
 po_expire_data = {}
@@ -19,9 +20,9 @@ def add_business_days(start_date, num_days):
             num_days -= 1
     return current_date
 
-# Generate SQL UPDATE statements for each line in sql_fetched.csv
-update_statements = []
-with open('updateexpire.csv', 'r') as sql_fetched_file:
+# Generate a single SQL UPDATE query using CASE statement with new lines for each entry
+update_cases = []
+with open('expired.csv', 'r') as sql_fetched_file:
     sql_fetched_reader = csv.DictReader(sql_fetched_file)
     for row in sql_fetched_reader:
         customer_name = row['customer_name']
@@ -30,14 +31,19 @@ with open('updateexpire.csv', 'r') as sql_fetched_file:
             order_time = datetime.datetime.strptime(row['order_time'], '%Y-%m-%d %H:%M:%S')
             po_expiry_date = add_business_days(order_time, po_expire_days)
             po_expiry_date_str = po_expiry_date.strftime('%Y-%m-%d %H:%M:%S')
-            update_statement = f"UPDATE preorder SET po_expired = '{po_expiry_date_str}' WHERE no_SO = '{row['no_SO']}';\n/"
-            update_statements.append(update_statement)
+            update_case = f"  WHEN no_SO = '{row['no_SO']}' THEN '{po_expiry_date_str}'"
+            update_cases.append(update_case)
 
-# Combine the update statements into a single transaction
-transaction = "\n".join(update_statements) + "\n"
+if update_cases:
+    case_statement = "\n".join(update_cases) + "\n  ELSE po_expired END"
+    single_update_query = f"UPDATE preorder SET po_expired = CASE{case_statement};"
+    transaction = single_update_query + "\n"
 
-# Copy the generated transaction to the clipboard
-pyperclip.copy(transaction)
+    # Copy the generated transaction to the clipboard
+    pyperclip.copy(transaction)
 
-print("Transaction with update statements has been generated and copied to the clipboard.")
-print("You can now paste the transaction wherever you need.")
+    print("Single transaction with a CASE statement (with new lines for entries) has been generated and copied to the clipboard.")
+    print("You can now paste the transaction wherever you need.")
+    os.remove('expired.csv')
+else:
+    print("No eligible records found in the input data.")
