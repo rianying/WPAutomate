a97faff5e743a8dd69d36b36ce9c1325715655cd
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2023-11-13 06:00:38 +0700

    Update env.py

--- env/env.py
+++ env/env.py
@@ -20,7 +20,7 @@ balikan_sf = {
 
 twilio_env = {
     "twilio_account_sid" : "AC31f35ab1b7e268a8b1b0147ac9ac012c",
-    "twilio_auth_token" : "fc3b10448588d1597c84bd9209559294",
+    "twilio_auth_token" : "1e6e8ebc4fcf84d2f98e5a1c36b07e2c",
     "twilio_phone_number" : "whatsapp:+14155238886",
     "to_phone_number" : "whatsapp:+6281282847410",
     "url" : "https://docs.google.com/spreadsheets/d/1pcjYibnJNEp1jow9mpJSb63-fyGkOAodVwL1WblVtpM/edit#gid=642080019"
